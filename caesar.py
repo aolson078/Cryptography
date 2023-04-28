7640b4557e8cd42d06d673def445c7d533ffa30f
Alex Olson <69769089+aolson078@users.noreply.github.com>
2023-04-28 10:27:47 -0400

    Replaced for loop with list comprehension

--- caesar.py
+++ caesar.py
@@ -1,27 +1,16 @@
 # Written by Alexander Olson
-# Draft: 1
+# Draft: 2
 
 def caesar():
-    clear  = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out."
-    cipher = ""
-    shift = 12
-    for i in range(len(clear)):
-
-        # If the char is uppercase and needs to loop back to 'A'
-        if (ord(clear[i]) + shift) >= 91 and chr(ord(clear[i])).isupper():
-          cipher += chr(65 + ((ord(clear[i]) + shift) - 91))
-
-        # If the char is lowercase and needs to loop back to 'a'
-        elif (ord(clear[i]) + shift) >= 123 and chr(ord(clear[i])).islower():
-          cipher += chr(97 + ((ord(clear[i]) + shift) - 123))
-
-        elif clear[i] == " ":
-          cipher += " "
-
-        else:
-            cipher += chr(((ord(clear[i]) + shift)))
-
-    return cipher
+	clear = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the " \
+	        "letters of the alphabet, that not a word could be made out. "
+	shift = 12
+	cipher = "".join([
+		chr((ord(c) - 65 + shift) % 26 + 65) if c.isupper()
+		else chr((ord(c) - 97 + shift) % 26 + 97) if c.islower()
+		else c for c in clear
+	])
+	return cipher
 
 
 print(caesar())
