cddf5a0533678359b5d1ffae96be43c71a16e1cf
Alexander Olson <aolson078@gmail.com>
2023-04-28 10:36:09 -0400

    Replaced list comprehension with byte array

--- caesar.py
+++ caesar.py
@@ -5,11 +5,15 @@ def caesar():
 	clear = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the " \
 	        "letters of the alphabet, that not a word could be made out. "
 	shift = 12
-	cipher = "".join([
-		chr((ord(c) - 65 + shift) % 26 + 65) if c.isupper()
-		else chr((ord(c) - 97 + shift) % 26 + 97) if c.islower()
-		else c for c in clear
-	])
+
+	# Using a byte array is a more efficient way to process strings
+	clear_bytes = bytearray(clear.encode())
+	for i, b in enumerate(clear_bytes):
+		if 65 <= b <= 90:
+			clear_bytes[i] = (b - 65 + shift) % 26 + 65
+		elif 97 <= b <= 122:
+			clear_bytes[i] = (b - 97 + shift) % 26 + 97
+	cipher = clear_bytes.decode()
 	return cipher
 
 
