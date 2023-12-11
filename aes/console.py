1ba7247f3093cdb9e0fb0566006f528ee5c7d7c7
Alex Olson <69769089+aolson078@users.noreply.github.com>
2023-12-11 08:58:53 -0500

    Update console to work with OO aes

--- aes/console.py
+++ aes/console.py
@@ -1,47 +1,40 @@
 from rich import print
-from rich import inspect
 from rich.console import Console
 from rich.theme import Theme
-from rich.table import Table
-import aes128
-
+from aes128 import AES
 
 """Console and Theme settings"""
-custom_theme = Theme( {"info": "bold cyan", "warning": "magenta", "danger": "bold red"})
+custom_theme = Theme({"info": "bold cyan", "warning": "magenta", "danger": "bold red"})
 console = Console(theme=custom_theme)
 
 
 def main():
-    
-    console.print(("Please choose from the following menu: \n"
-                  + "1: Run AES128\n"
-                  + "2: Exit Program\n"), style="info")
-    
-    choice = int(input())
+	console.print(("Please choose from the following menu: \n"
+	               + "1: Run AES128\n"
+	               + "2: Exit Program\n"), style="info")
 
-    print()
+	choice = int(input())
+	print()
 
-    if choice == 1:
-        # Take user input for clear text
-        clear_text = [[0x0F, 0x0E, 0x0D, 0x0C], [0x0B, 0x0A, 0x09, 0x08], [0x07, 0x06, 0x05, 0x04], [0x03, 0x02, 0x01, 0x00]]
-        state = aes128.initialize_state(clear_text)
-        
-        # Key will be 128 bits for AES-128
-        key = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F]
-        subkeys = aes128.generate_subkeys(key)
+	if choice == 1:
+		# Take user input in clear text for state
+		aes = AES(
+			[[0xFF, 0x0E, 0x0D, 0x0C], [0x0B, 0x0A, 0x09, 0x08], [0x07, 0x06, 0x05, 0x04], [0x03, 0x02, 0x01, 0x00]])
+		key = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F]
+		subkeys = aes.generate_subkeys(key)
 
-        # Initial key addition
-        state = aes128.add_round_key(state, subkeys[0])
+		# Initial key addition
+		aes.state = aes.add_round_key(subkeys[0])
 
-        for i in range(9):
-            state = aes128.add_round_key(aes128.round_layers(state), subkeys[i+1])
+		for i in range(9):
+			aes.round_layers()
+			aes.state = aes.add_round_key(subkeys[i + 1])
+		aes.last_round()
+		aes.state = aes.add_round_key(subkeys[-1])
+		return aes.state
 
-        state = aes128.add_round_key(aes128.last_round(state), subkeys[-1])
-
-        return state
-    
-    elif choice == 2:
-        exit()
+	elif choice == 2:
+		exit()
 
 
 print(main())
