29a5c2983dbd23e0bc06ed9e2396399c8690fd90
Alex Olson <69769089+aolson078@users.noreply.github.com>
2023-12-22 10:25:25 -0500

    Create block_mode.py
    Currently only has ECB mode with Ansi X9.23 padding

---
+++ aes/block_mode.py
@@ -0,0 +1,61 @@
+# DO NOT USE FOR ACTUAL SECURITY/PRODUCTION, NOT CRYPTOGRAPHICALLY SECURE!!!!!!!!!!!!!!!!
+# Written by Alex Olson
+# This file contains the block modes of operation to run aes128
+from math import ceil
+import console
+
+# Function takes 2d list (of size 1) as input and outputs string of hex values
+def to_hex(string):
+	output = ""
+	for i in string:
+		for j in i:
+			if len(hex(j)[2::]) == 1:
+				output += "0"
+				output += (hex(j)[2::])
+			else:
+				output += (hex(j)[2::])
+	return output
+
+
+def ecb():
+	# Take user input (char string)
+	plaintext = input("Please enter plaintext to encrypt: ")
+
+	# Calculate number of blocks needed
+	num_blocks = ceil(len(plaintext) / 16)
+	num_padding = 16 - len(plaintext) % 16
+
+	# Add padding (pad with same value as number of padding bytes)
+	blocks = []
+	for i in range(num_blocks):
+		blocks.append([])
+	print("Blocks:")
+	for block in blocks:
+		while len(block) < 16 and plaintext:
+			# Appends each letters ordinal ascii value
+			block.append(ord(plaintext[0]))
+			plaintext = plaintext[1::]
+
+	# Pad blocks with Ansi X9.23 padding (Pad will all 0s then w/ num_padding in last position
+	while len(blocks[-1]) < 16:
+		blocks[-1].append(0)  # Pad with 0s
+	blocks[-1][-1] = num_padding
+
+	# 2D list to hold all encrypted blocks
+	full_output = []
+	# Run aes128 on each block
+	for block in blocks:
+		text = [block[:4], block[4:8], block[8:12], block[12:16]]
+		output = ""
+		cipher = console.encrypt(text)
+		for i in cipher:
+			for j in i:
+				output += (chr(j))
+		cipher_hex_output = to_hex(cipher)
+		full_output.append(cipher_hex_output)
+
+	# Return final value
+	print(full_output)
+
+
+ecb()
