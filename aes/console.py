e70fd197ee510c408c8907341619a4ad4769be11
Alex Olson <69769089+aolson078@users.noreply.github.com>
2023-12-24 09:13:41 -0500

    Moved ECB mode selection into console

--- aes/console.py
+++ aes/console.py
@@ -1,26 +1,15 @@
 # DO NOT USE FOR ACTUAL SECURITY/PRODUCTION, NOT CRYPTOGRAPHICALLY SECURE!!!!!!!!!!!!!!!!
 # Written by Alex Olson
 from encrypt import encrypt, decrypt
+from block_mode import ecb
 import ast
 import re
 
 KEY = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]
 
+# Checks if value is in hex
 def is_hexadecimal(s):
 	return bool(re.fullmatch(r'[0-9a-fA-F]+', s))
-
-
-# Function takes 2d list (of size 1) as input and outputs string of hex values
-def to_hex(string):
-	output = ""
-	for i in string:
-		for j in i:
-			if len(hex(j)[2::]) == 1:
-				output += "0"
-				output += (hex(j)[2::])
-			else:
-				output += (hex(j)[2::])
-	return output
 
 
 # Takes 16 bytes of string input, returns it as a 4x4 2d list
@@ -44,7 +33,11 @@ def format_output(cipher):
 	output = ""
 	for i in cipher:
 		for j in i:
-			output += (hex(j)[2:])
+			if len(hex(j)) == 4:
+				output += (hex(j)[2:])
+			else:
+				output += "0"
+				output += (hex(j)[2:])
 	return output
 
 
@@ -82,16 +75,19 @@ def main():
 	choice = int(input())
 
 	if choice == 1:
-		plaintext = format_input(input("Please enter a 16 byte plaintext as string to encrypt: "))
-		print(encrypt(plaintext, KEY))
+		block_choice = int(input("Which block mode would you like to use? \n " + " 1: ECB Mode "))
+		if block_choice == 1:
+			plaintext = format_input(input("Please enter a 16 byte plaintext as string to encrypt: "))
+			ecb(plaintext, KEY)
 
 	if choice == 2:
 		# Decrypt takes a 2d list 4x4
-		cipher = input("Please enter ciphertext to decrypt (Either list of 16 values, or 1D string of list len 16): ")
-		# cipher = "251, 218, 206, 230, 241, 61, 176, 130, 212, 246, 13, 159, 243, 254, 29, 253"
-		cipher = normalize_input_for_decrypt(cipher)
-		print(cipher)
-		print(to_hex(decrypt(cipher, KEY)))
+		block_choice = int(input("Which block mode would you like to use? \n " + " 1: ECB Mode "))
+		if block_choice == 1:
+			cipher = input("Please enter ciphertext to decrypt (Either list of 16 values, or 1D string of list len 16): ")
+			cipher = normalize_input_for_decrypt(cipher)
+			ecb(cipher, KEY, enc=False)
+			print(format_output(decrypt(cipher, KEY)))
 
 	elif choice == 3:
 		exit()
